7197d482d80f1b4ef4267c67d635727d54719603
Ashwanth Kumar <ashwanth.kumar@avalara.com>
2020-07-05 11:36:56 +0530

    Keeping the debug logs for a while more

--- frontend/gcloud-apis/base.tsx
+++ frontend/gcloud-apis/base.tsx
@@ -58,7 +58,9 @@ export abstract class BaseClient {
   }
 
   private async handleResponse(response: Response): Promise<any> {
+    console.log(response);
     const responseAsJson = await response.json() as any | ErrorResponse;
+    console.log(responseAsJson);
     if (response.status !== 200) {
       // this is now an error
       throw responseAsJson;
